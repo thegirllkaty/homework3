5477870c00235af004fc5feebcb35687709b428d
theksenia <ksu.pm13@gmail.com>
2021-07-19 01:34:57 +0300

    add

--- 4.py
+++ 4.py
@@ -1,1 +1,8 @@
-print("hello")
+x = int(input('Enter A: '))
+y = int(input('Enter B: '))
+if x < y:
+    for i in range(x, y+1):
+        print(i)
+else:
+    for j in range(x, y-1, -1):
+        print(j)
