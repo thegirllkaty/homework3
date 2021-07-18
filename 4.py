0e89e8453d761bb21e9d58bbe05cdc6b2a3e0e7c
theksenia <ksu.pm13@gmail.com>
2021-07-19 02:04:47 +0300

    add

--- 4.py
+++ 4.py
@@ -4,5 +4,5 @@ if x < y:
     for i in range(x, y+1):
         print(i)
 else:
-    for j in range(x, y-1, -1):
+    for j in range(x, y-1, -1): # у-1 элемент идущий после последнего
         print(j)
